20178edfd4ee97ea881de729f353fd4df2d0765f
ABHISHEK TIWARI <tiwariabhishek27611@gmail.com>
2021-02-19 18:22:20 +0530

    tag changed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div id="root">
-            <p>I am learning React. My life is getting better.</p>
+           <p>I am learning React. My life is getting better.</p>
         </div>
     );
 };
