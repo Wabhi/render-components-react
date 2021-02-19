4c1a33415932e78e75cfe887cffe24d34b277928
ABHISHEK TIWARI <tiwariabhishek27611@gmail.com>
2021-02-19 19:34:20 +0530

    id changed

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,9 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div id="root">
            <p>I am learning React. My life is getting better.</p>
-        </div>
     );
 };
 
